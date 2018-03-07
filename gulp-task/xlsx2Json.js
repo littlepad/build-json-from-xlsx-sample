6953c43937dce8624fe9824aa5aa1e4718b25d4d
littlepad <littlepad@gmail.com>
2018-03-07 23:07:23 +0900

    add output list json task

--- gulp-task/xlsx2Json.js
+++ gulp-task/xlsx2Json.js
@@ -1,4 +1,11 @@
-const Parser = require('parse-xl');
+import Parser from 'parse-xl';
+import ProductList from './list/ProductList';
+import SellerList from './list/SellerList';
+import ProductJson from './json/ProductJson';
+import SellerJson from './json/SellerJson';
+
+const PRODUCT_SHEET = 'product';
+const SELLER_SHEET = 'seller';
 
 module.exports = {
   /**
@@ -10,8 +17,16 @@ module.exports = {
   /* eslint no-unused-vars: 1 */
   build: (xlsx, dist) => {
     const data = new Parser(xlsx);
+    const productList = new ProductList(data.data[PRODUCT_SHEET]);
+    const sellerList = new SellerList(data.data[SELLER_SHEET]);
+    const productJson = new ProductJson(productList);
+    const sellerJson = new SellerJson(sellerList);
+
+    productJson.output('json/product/');
+    sellerJson.output('json/seller/');
 
     /* eslint no-console: 0 */
-    console.log(data.data);
+    console.log(productList);
+    console.log(sellerList);
   }
 };
